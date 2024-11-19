32ace3963a160f951c870e08f59db1dc52dc88d7
bjkumar03 <bjkumar180@gmail.com>
2024-11-19 22:15:29 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   const [foodItem, setFoodItem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("https://y-27mh.onrender.com", {
+    let response = await fetch("https://y-3.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
